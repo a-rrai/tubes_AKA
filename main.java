7ea0e6b4f5d386c6cfb7752c169a0b9ea0da4b1b
Raissha Najwa Maharani <raissha.njw@gmail.com>
2026-01-10 02:37:39 +0700

    Update main.java
    ditambahin biar bisa menampilkan running time untuk tiap input

--- main.java
+++ main.java
@@ -2,16 +2,26 @@ package aka;
 
 public class main {
 
-	public static void main(String[] args) {
-		int[] data = {10, 100, 1000, 10000, 100000};
+    public static void main(String[] args) {
+        int[] ukuranInput = {10,100,1000,10000,100000};
         int nilaiPembanding = 57500;
 
         algoritma hitung = new algoritma();
-        int hasilIteratif = hitung.hitungIteratif(data, nilaiPembanding);
-		int hasilRekursif = hitung.hitungRekursif(data, nilaiPembanding, 0);
-        
-        System.out.println("Jumlah elemen > " + nilaiPembanding + " (iteratif): " + hasilIteratif);
-        System.out.println("Jumlah elemen > " + nilaiPembanding + " (rekursif): " + hasilRekursif);
+
+        long startIter = System.nanoTime();  
+        int hasilIteratif = hitung.hitungIteratif(ukuranInput, nilaiPembanding);
+        long endIter = System.nanoTime();    
+        double waktuIter = (endIter - startIter) / 1e6; 
+
+        System.out.println("Ukuran input > " + nilaiPembanding + " (iteratif): " + hasilIteratif);
+        System.out.println("Waktu eksekusi iteratif: " + waktuIter + " ms");
+
+        long startRek = System.nanoTime();
+        int hasilRekursif = hitung.hitungRekursif(ukuranInput, nilaiPembanding, 0);
+        long endRek = System.nanoTime();
+        double waktuRek = (endRek - startRek) / 1e6; 
+
+        System.out.println("Ukuran input > " + nilaiPembanding + " (rekursif): " + hasilRekursif);
+        System.out.println("Waktu eksekusi rekursif: " + waktuRek + " ms");
     }
 }
-
